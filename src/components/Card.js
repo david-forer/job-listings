1ccc96d1bfd804bce197330599207faf7b940645
David J Forer <david.j.forer@gmail.com>
2021-10-30 18:22:26 +0900

    first commit

---
+++ src/components/Card.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./card.css"
+
+const Card = () => {
+  return (
+    <div className="card-grid">
+      <div className="card-grid__list-item">
+        <h2>Card Header</h2>
+        <small>Card Body</small>
+      </div>
+    </div>
+  )
+}
+
+export default Card
